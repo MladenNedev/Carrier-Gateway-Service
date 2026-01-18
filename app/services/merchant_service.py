04460504dcb5336bf2b8227b4a762dadd3b44881
Mladen nedev <mladen.nnedev@gmail.com>
2026-01-18 20:20:05 +0100

    feat(services): add merchant list and read logic

--- app/services/merchant_service.py
+++ app/services/merchant_service.py
@@ -1,6 +1,8 @@
 from app.domain.merchant import Merchant
 from app.persistance.repositories import MerchantRepository
 from app.persistance.models import MerchantModel
+from app.domain.merchant import Merchant
+from uuid import UUID
 
 class MerchantService:
     def __init__(self, repo: MerchantRepository):
@@ -18,3 +20,16 @@ class MerchantService:
             id=saved.id,
             name=saved.name
         )
+
+    def get_merchant(self, merchant_id: UUID) -> Merchant:
+        model = self.repo.get_by_id(merchant_id)
+        if not model:
+            raise ValueError(f"Merchant with id {merchant_id} does not exist")
+
+        return Merchant(id=model.id, name=model.name)
+
+    def list_merchants(self) -> list[Merchant]:
+        return [
+            Merchant(id=m.id, name=m.name)
+            for m in self.repo.list()
+        ]
